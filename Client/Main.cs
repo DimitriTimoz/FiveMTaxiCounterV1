de39ebe1e37e8777d078cfbf8cec4e16f79b24fd
SuperChocolatine20 <45998396+SuperChocolatine20@users.noreply.github.com>
2019-07-27 23:31:16 +0200

    Add files via upload

---
+++ Client/Main.cs
@@ -0,0 +1,122 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.UI;
+using System;
+using System.Collections.Generic;
+
+namespace Client
+{
+    public class Main : BaseScript
+    {
+        //Prix du kilomètre
+        private double perKilometer = 10;
+        //Prix minimum
+        private int min = 5;
+
+        //prix du kilometre le jour (2 mins IRL)
+        private double perGTAHourDay = 3;
+        //prix du kilometre la nuit (2 mins IRL)
+        private double perGTAHourNight = 5;
+
+        //Vitesse moyenne
+        private double averageV = 0;
+        //Adition des vitesses
+        private double total = 0;
+        //Prix
+        private int price = 0;
+
+        //Formule jour/nuit
+        bool night = false;
+        //Calcul le prix ?
+        bool started = false;
+        //Repètre temps 1/2 sec
+        int time = 0;
+
+
+
+        public Main()
+        {
+            EventHandlers["TaxiCounter:Client:StartCounter"] += new Action(Start);
+            EventHandlers["TaxiCounter:Client:ClearCounter"] += new Action(Clear);
+            EventHandlers["TaxiCounter:Client:ResumeCounter"] += new Action(Resume);
+            EventHandlers["TaxiCounter:Client:StopCounter"] += new Action(Stop);
+            EventHandlers["TaxiCounter:Client:ChooseNightTarif"] += new Action(ChooseNightTarif);
+            EventHandlers["TaxiCounter:Client:ChooseDayTarif"] += new Action(ChooseDayTarif);
+            EventHandlers["TaxiCounter:Client:Pay"] += new Action(Pay);
+        }
+
+        private void ChooseNightTarif()
+        {
+            night = true;
+        }
+        private void ChooseDayTarif()
+        {
+            night = false;
+        }
+        private void Start()
+        { 
+            Clear();
+            started = true;
+            Calcul();
+        }
+
+        private void Resume()
+        {
+            started = true;
+            Calcul();
+        }
+        private void Clear()
+        {
+            total = 0;
+            time = 0;
+            price = 0;
+        }
+
+        private void Stop()
+        {
+            started = false;
+        }
+
+        private async void Calcul()
+        {
+            while (true)
+            {
+                await Delay(50);
+                Player pl = new Player(1);
+                Vehicle vl = pl.LastVehicle;
+                if (!started)
+                {
+                    return;
+                }
+
+                //Gére le temps
+                time++;
+
+                //Calcul la vitesse moyenne
+                total += (int)((vl.Speed) * 3.6);
+                averageV = total / time;
+
+                // V x T
+                double distance = averageV * time / 3600 / 20;
+
+                //Calcule le prix (Prix minimum + Prix du kilometre x distance + Minutes depuis le début x Prix de l'heure)
+                if (night)
+                {
+                    price = (int)(min + perKilometer * distance + (time / 600 * perGTAHourNight));
+                }
+                else
+                {
+                    price = (int)(min + perKilometer * distance + (time / 600 * perGTAHourDay));
+                }
+                
+                //Affiche le prix 
+                Screen.ShowSubtitle("~b~Prix de la course: ~g~" + price.ToString() + " $", 51); 
+            }
+        }
+        private void Pay()
+        {
+            //Fonction système de payement         
+        }
+
+        
+    }
+}
---
+++ Server/Main.cs
@@ -0,0 +1,55 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+
+
+namespace Server
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+            EventHandlers["chatMessage"] += new Action<int, string, string>(OnChatMessage);
+        }
+        private void OnChatMessage(int src, string color, string msg)
+        {
+            Player player = new PlayerList()[src];
+            string[] args = msg.Split(' ');
+            if(args[0] == "!start")
+            {           
+                player.TriggerEvent("TaxiCounter:Client:StartCounter");
+                API.CancelEvent();
+            }
+            else if (args[0] == "!clear")
+            {
+                player.TriggerEvent("TaxiCounter:Client:ClearCounter");
+                API.CancelEvent();
+            }
+            else if (args[0] == "!stop")
+            {
+                player.TriggerEvent("TaxiCounter:Client:StopCounter");
+                API.CancelEvent();
+            }
+            else if (args[0] == "!resume")
+            {
+                player.TriggerEvent("TaxiCounter:Client:ResumeCounter");
+                API.CancelEvent();
+            }
+            else if (args[0] == "!day")
+            {
+                player.TriggerEvent("TaxiCounter:Client:ChooseDayTarif");
+                API.CancelEvent();
+            }
+            else if (args[0] == "!night")
+            {
+                player.TriggerEvent("TaxiCounter:Client:ChooseNightTarif");
+                API.CancelEvent();
+            }
+            else if (args[0] == "!pay")
+            {
+                player.TriggerEvent("TaxiCounter:Client:Pay");
+                API.CancelEvent();
+            }
+        }
+    }
+}
